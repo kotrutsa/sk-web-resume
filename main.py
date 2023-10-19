d6de0618648b05cb2ede6ed013481eb08231d146
kotrutsa <vcotruta95@gmail.com>
2023-10-19 17:48:42 +0200

    Second commit

--- main.py
+++ main.py
@@ -1,24 +1,14 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 
 app = Flask(__name__)
 Bootstrap5(app)
 
+
 @app.route('/')
 def home():
     return render_template("index.html")
 
-@app.route('/resume')
-def resume():
-    return render_template("resume.html")
-
-@app.route('/projects')
-def projects():
-    return render_template("projects.html")
-
-@app.route('/contact')
-def contact():
-    return render_template("contact.html")
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
